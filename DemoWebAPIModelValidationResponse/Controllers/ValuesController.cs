5fe0abf6e0f2b0ec0369bd3e92fb18020a7442f7
Poy Chang <poypost@gmail.com>
2019-11-21 11:18:00 +0800

    inti project

---
+++ DemoWebAPIModelValidationResponse/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebAPIModelValidationResponse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Demo-WebAPI-Model-Validation-Response
